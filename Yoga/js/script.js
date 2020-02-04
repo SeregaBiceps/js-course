0dbfe1f038cdc06fff0d3723e216ed87aac41be3
Igor <npoqpu2010@mail.ru>
2020-02-04 13:31:56 +0500

    Ninth task

--- Yoga/js/script.js
+++ Yoga/js/script.js
@@ -93,4 +93,22 @@ window.addEventListener('DOMContentLoaded', function () { // после прог
     }
 
     setClock('timer', deadLine); // начать цикл функций с переменными id таймера и конечным временем
+
+    // Modal
+
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close');
+
+    more.addEventListener('click', function() {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    });
+
+    close.addEventListener('click', function() {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    });
 });
